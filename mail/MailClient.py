66021f19b1fe25dd416ebd5bed976266c9b80309
govindan <gowindone.ns@gmail.com>
2015-07-25 21:56:38 +0530

    mail client

---
+++ mail/MailClient.py
@@ -0,0 +1,26 @@
+def send_email():
+            import smtplib
+
+            gmail_user = "gowindone.ns@gmail.com"
+            gmail_pwd = ""
+            FROM = 'gowindone.ns@gmail.com'
+            TO = ['gowindone@rediffmail.com','govindan.n@hcl.com'] #must be a list
+            SUBJECT = "Testing sending using gmail"
+            TEXT = "Testing sending mail using gmail servers"
+
+            # Prepare actual message
+            message = """\From: %s\nTo: %s\nSubject: %s\n\n%s
+            """ % (FROM, ", ".join(TO), SUBJECT, TEXT)
+            try:
+                #server = smtplib.SMTP(SERVER) 
+                server = smtplib.SMTP("smtp.gmail.com", 587) #or port 465 doesn't seem to work!
+                server.ehlo()
+                server.starttls()
+                server.login(gmail_user, gmail_pwd)
+                server.sendmail(FROM, TO, message)
+                #server.quit()
+                server.close()
+                print 'successfully sent the mail'
+            except:
+                print "failed to send mail"
+send_email()
